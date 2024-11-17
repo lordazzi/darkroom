65047f458322bb35549d9a36e75b67886e5b5edd
Ricardo Azzi Silva <ricardoazzi91@hotmail.com>
2024-11-17 22:27:21 +0000

    incluindo titulos

--- src/app/pages/chating/header-title.factory.ts
+++ src/app/pages/chating/header-title.factory.ts
@@ -4,12 +4,12 @@ import { Gender } from '../../domain/gender.enum';
 @Injectable()
 export class HeaderTitleFactory {
 
-  private [Gender.MALE] = ['guy','dude','man','pal','male','mister','lord', 'potential husband'];
-  private [Gender.FEMALE] = ['woman','lady','female','miss','dame','madam','missy','potential wife'];
-  private both = ['partner','person','human','person'];
+  private [Gender.MALE] = ['guy','dude','man','pal','male','mister','lord', 'potential husband', 'potential boyfriend'];
+  private [Gender.FEMALE] = ['woman','lady','female','miss','dame','madam','missy','potential wife', 'potential girlfriend'];
+  private both = ['partner','person','human','person', 'crush', 'match'];
   private advice = ['kind','nice','fine','pretty','cool','tender','lovely','good','friendly','awesome','great','gentle','positive','gracious','cordial'];
 
-  getTitle(genderFound: Gender) {
+  getTitle(genderFound: Gender): string {
     const whos = new Array<string>().concat(this[genderFound]).concat(this.both);
     const who = whos[Math.floor(Math.random() * whos.length)];
     const adjective = this.advice[Math.floor(Math.random() * this.advice.length)];
